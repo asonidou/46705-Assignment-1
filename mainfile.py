80010618be4f2123b7e88e83f5686bbfb6fb2350
asonidou <s250818@student.dtu.dk>
2025-02-18 08:54:16 +0100

    Add files via upload

---
+++ mainfile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 18 08:33:55 2025
+
+@author: alexandrasonidou
+"""
+
+import numpy as np
+import ReadNetworkData as rd
+filename='TestSystem.txt'
+
