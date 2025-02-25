a0fd7ade4c0f595b2e3853ebc4c6fcb61c3f69e4
s250374 <s250374@student.dtu.dk>
2025-02-25 12:01:01 +0100

    Messy code with TA help

--- mainfile.py
+++ mainfile.py
@@ -29,19 +29,62 @@ def LoadNetworkData (filename):
     M_trans = len(tran_data)
     M_branches = M_lines + M_trans
     Ybus = np.zeros((N,N),dtype=complex)
+    
     print(line_data)
 
     # Continue with your code here and create the needed data types...
     # sweep over the bus_data,load_data,gen_data,line_data, tran_data to fill in appropriate values
 
     # Line Impedances => Line Admittances and susceptance
-    Za=1j*line_data[0][4]+line_data[0][3];  Ya=1/Za;  b_a= 1j*line_data[0][5]/2
-    Zb=1j*line_data[1][4]+line_data[1][3];  Yb=1/Zb;  b_b= 1j*line_data[1][5]/2
-    Zc=1j*line_data[2][4]+line_data[2][3]; Yc=1/Zc;  b_c= 1j*line_data[2][5]/2
+    Z = np.zeros(N,dtype=complex)
+    Y = np.zeros(N,dtype=complex)
+    b = np.zeros(N,dtype=complex)
+    Bus_nr = np.zeros(N)
+    fb = np.zeros(N)
+    tb = np.zeros(N)
 
-    # N = 3 #number of busses
+    for line in line_data:
+        (FROM_BUS, TO_BUS, ID, R, X, B, MVA_rating, X2, X0) = line
+        from_idx = bus_to_ind[FROM_BUS]
+        to_idx = bus_to_ind[TO_BUS]
+        Z = R
+        Y = 1/Z
+        Ybus[from_idx,to_idx] += -Y
+        Ybus[from_idx,from_idx] += Y + 
 
-    #Bus addmitance Matrix
+
+    for tran in tran_data:
+        (FROM_BUS, TO_BUS, ID, R, X, n, ANG1, FROM_CON, TO_CON, MVA_rating, X2, X0) = tran
+        from_idx = bus_to_ind[FROM_BUS]
+        to_idx = bus_to_ind[TO_BUS]
+        ....
+
+
+
+    for l in range(N):
+        Z[l] = 1j*line_data[l][4]+line_data[l][3]
+        Y[l] = 1/Z[l]
+        b[l] = 1j*line_data[l][5]/2
+        Bus_nr[l] = bus_data[l][0]
+        fb[l] = line_data[l][0]  # Store value and its original index
+        tb[l] = line_data[l][1]
+
+    #Bus addmitance Matrix Ybus
+
+    for r in range(N):
+        for c in range(N):
+            from_idx = np.where(fb == Bus_nr[r])[0]
+            to_idx = np.where(tb == Bus_nr[r])[0] 
+            
+            # Off-diagonal elements (mutual admittance)
+            Ybus[fb, tb] -= Y[r]
+            Ybus[tb, fb] -= Y[r]
+            
+            # Diagonal elements (self-admittance)
+            Ybus[fb, fb] += Y[r] + 1j * b[r] / 2
+            Ybus[tb, tb] += Y[r] + 1j * b[r] / 2
+
+
     Ybus=np.array([[Ya+Yc+b_a+b_c ,-Ya           ,-Yc           ],\
                    [-Ya           , Ya+Yb+b_a+b_b,-Yb           ],\
                   [-Yc           ,-Yb           ,  Yb+Yc+b_b+b_c]])
